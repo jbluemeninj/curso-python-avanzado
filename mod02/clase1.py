96088280a045612ccad112f342cb228de79340f9
jbluemeninj <jbluemeninj@hotmail.com>
2025-04-06 09:03:51 -0500

    comenzando el modulo 2

---
+++ mod02/clase1.py
@@ -0,0 +1,27 @@
+"""
+PROGRAMA PARA GUARDAR DATOS DE MI PC
+"""
+import platform,socket
+
+def guardar_pc_info():
+
+    pc_data = "=== INFO PC ===" + "\n"
+    pc_data += "SO : " + platform.system() + "\n"
+    pc_data += "VERSION : " + platform.release() + "\n"
+    pc_data += "VERSION : " + platform.version() + "\n"
+    pc_data += "ARQUITECTURA : " + platform.machine() + "\n"
+    pc_data += "NOMBRE : " + socket.gethostname() + "\n"
+    pc_data += "IP : " + socket.gethostbyname(socket.gethostname()) + "\n"
+    pc_data += "=== FIN INFO PC ===" + "\n"
+
+    pc_file = open("pc.txt","w")
+    pc_file.write(pc_data)
+    pc_file.close()
+
+    print("Se guardo con exito!!!")
+
+def leer_pc_info():
+    pass
+
+if __name__ == "__main__":
+    guardar_pc_info()
